b28a80802c1737bb18019b8c7e86a3970058e653
buyeolim <buyeolim@gmail.com>
2023-04-05 03:25:26 +0900

    과제03: 인기곡 차트 구현 추가(#1)

--- src/models/Song.js
+++ src/models/Song.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const songSchema = new mongoose.Schema({});
+const songSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  artist: { type: String, required: true, trim: true },
+  // fileUrl: { type: String, required: true },
+  // thumbUrl: { type: String, required: true },
+  // date: { type: Date, required: true },
+  streams: { type: Number, required: true, default: 0 },
+  rank: { type: Number },
+});
 
 const Song = mongoose.model("Song", songSchema);
 
